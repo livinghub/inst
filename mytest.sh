a4e7929b9f1b19a3f31b0483806dad29f5472d85
lyh <707884308@qq.com>
2022-01-13 16:38:03 +0800

    增加论文电路

--- mytest.sh
+++ mytest.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-dirPath="/home/lyh/Desktop/inst_16/"
-exc="/home/lyh/qfr/build/qfr_example"
-num="*_100_*"
-a=0
-#files=$(ls $dirPath$folder$num)
-files=$(ls $dirPath$num)
+dirPath="./paper_cirucit/"
+exc="./qfr_example_lsv2"
+files=$(ls $dirPath$folder)
+
 for file in $files
 do
-#    sleep 5 &
-#    echo $file
-    timeout 2h $exc $file >> "/home/lyh/Desktop/16_out/$a.txt" &
-    ((a++))
+  echo $exc $dirPath$file
+#  timeout 1h $exc $dirPath$file
+    timeout 1h $exc $dirPath$file >> "./lsv2_out.txt" &
 done
